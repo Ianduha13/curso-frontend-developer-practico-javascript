1ab53edd5b54d0886a23ff5f16574b5a349095ad
Ianduha13 <ianduhamelhayes@gmail.com>
2022-07-22 10:52:22 -0300

    Carrito vinculado

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector ('.menu');
+const mobileMenu = document.querySelector ('.mobile-menu');
+const productDetail= document.querySelector('.product-detail');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 
-menuEmail.addEventListener('click',toggleDesktopMenu);
+
+
+menuEmail.addEventListener('click',toggleDesktopMenu);// kevin es muy lindo
+burgerMenu.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
+
 
 function toggleDesktopMenu () {
 desktopMenu.classList.toggle('inactive');
 }
+function toggleMobileMenu () {
+    mobileMenu.classList.toggle('inactive');
+    }
+function toggleCarritoAside (){
+    const isMobileMenuOpen = !mobileMenu.classList.contains('inactive')
+    if(isMobileMenuOpen){
+        mobileMenu.classList.add('inactive')
+    }
+    productDetail.classList.toggle('inactive');
+   
+    }
